03d446829cbf3c7b0ef7812951addaf570344a2d
EmmanuelFlorencioT <85115613+EmmanuelFlorencioT@users.noreply.github.com>
2022-05-27 19:06:50 -0500

    Added a message actor to print the final score in GameOver

--- NewAsteroids/Message.java
+++ NewAsteroids/Message.java
@@ -1,38 +1,30 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Score here.
+ * Write a description of class Message here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Score extends Actor
+public class Message extends Actor
 {
     private int score;
     /**
-     * Act - do whatever the Score wants to do. This method is called whenever
+     * Act - do whatever the Message wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act()
     {
         // Add your action code here.
-        updateScore();
+        updateMessage();
     }
     
-    public Score(){
-        this.score = 0;
+    public Message(int score){
+        this.score = score;
     }
     
-    public void updateScore(){
+    public void updateMessage(){
         String formattedScore = String.format("%05d", this.score);
         setImage(new GreenfootImage("Score: " + formattedScore, 20, Color.CYAN, Color.BLACK));
-    }
-    
-    public void increaseScore(int score){
-        this.score += score;
-    }
-    
-    public int getCurrScore(){
-        return this.score;
     }
 }
